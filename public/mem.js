1d700434bab39f8a7fc85a77a1626a602b3fb5e2
Greg Ward <codenlighten1@gmail.com>
2022-12-22 18:15:24 -0500

    first

--- public/mem.js
+++ public/mem.js
@@ -84,6 +84,7 @@ const getMem = (phrase) => {
 								"mem"
 							).innerHTML = `<h2>Message:</br>${f2}</h2><h2>Sender:<br/>${paymail}</h2><h2>AppID: ${f1}</h2><h2>Hash:</br>${hash}</h2><h2>TXID:</br>${url}</h2>`;
 						}
+						document.getElementById("mem").style.padding = "5px";
 					}
 				}
 			}
