cfd9778002ca80e815a421ef0a91952de9817df2
Greg Ward <codenlighten1@gmail.com>
2022-12-23 16:43:47 -0500

    first

--- public/mem.js
+++ public/mem.js
@@ -81,12 +81,13 @@ const getMem = (phrase) => {
 					if (encryption == "true") {
 						try {
 							f2 = decrypt(localStorage.decryption, f2);
-
-							document.getElementById(
-								"mem"
-							).innerHTML = `<h2>Message:</br>${f2}</h2><h2>Sender:<br/>${paymail}</h2><h2>AppID: ${f1}</h2><h2>Hash:</br>${hash}</h2><h2>TXID:</br>${url}</h2>`;
-							document.getElementById("mem").style.padding = "5px";
-							playSound();
+							if (!f2.includes("=")) {
+								document.getElementById(
+									"mem"
+								).innerHTML = `<h2>Message:</br>${f2}</h2><h2>Sender:<br/>${paymail}</h2><h2>AppID: ${f1}</h2><h2>Hash:</br>${hash}</h2><h2>TXID:</br>${url}</h2>`;
+								document.getElementById("mem").style.padding = "5px";
+								playSound();
+							}
 						} catch (e) {
 							console.log(e);
 						}
