107faf2dd4578c3ccf024417b7a4e89fe32c9827
Greg Ward <codenlighten1@gmail.com>
2022-12-23 21:49:00 -0500

    first

--- public/mem.js
+++ public/mem.js
@@ -97,13 +97,6 @@ const getMem = (phrase) => {
 						} catch (e) {
 							console.log(e);
 						}
-						// if (f2 != "") {
-						// 	playSound();
-						// 	document.getElementById(
-						// 		"mem"
-						// 	).innerHTML = `<h2>Message:</br>${f2}</h2><h2>Sender:<br/>${paymail}</h2><h2>AppID: ${f1}</h2><h2>Hash:</br>${hash}</h2><h2>TXID:</br>${url}</h2>`;
-						// }
-						// document.getElementById("mem").style.padding = "5px";
 					}
 				}
 			}
