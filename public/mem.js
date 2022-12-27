3ada3ca74025115bf1c6252469abe57b6a570ec6
Greg Ward <codenlighten1@gmail.com>
2022-12-27 09:36:28 -0500

    first

--- public/mem.js
+++ public/mem.js
@@ -81,7 +81,7 @@ const getMem = async (phrase) => {
 					// console.log(checked);
 					if (encryption == "true" && encryptHash == localHash) {
 						try {
-							const messageItem = document.createElement("h6");
+							const messageItem = document.createElement("h5");
 							f2 = await decrypt(localStorage.decryption, f2);
 							paymail = await decrypt(localStorage.decryption, paymail);
 							messageItem.innerHTML = `$${paymail}: ${f2}`;
